a232f15e94e9246888fd8fe4702414d56692d0d3
astro <priyam.elitekods@gmail.com>
2024-04-22 15:28:15 +0530

    env use in project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>API URL: {process.env.REACT_APP_API_URL}</p>
         </p>
         <a
           className="App-link"
